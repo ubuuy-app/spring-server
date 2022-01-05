1ed15a8274331e6a7fce116c60f99d5a700d4b0b
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-01-05 18:41:17 +0200

    initial commit

---
+++ src/main/java/com/aviobrief/springserver/SpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.aviobrief.springserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerApplication.class, args);
+    }
+
+}
