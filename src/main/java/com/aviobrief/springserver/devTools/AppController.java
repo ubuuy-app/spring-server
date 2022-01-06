36bf61e8d63cdb8321ec8b4b6743f1e6ee11c520
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-01-06 11:06:34 +0200

    extracted message as constant

--- src/main/java/com/aviobrief/springserver/devTools/AppController.java
+++ src/main/java/com/aviobrief/springserver/devTools/AppController.java
@@ -1,17 +1,22 @@
 package com.aviobrief.springserver.devTools;
 
+import com.aviobrief.springserver.utils.log.LoggerMessages;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+import static com.aviobrief.springserver.utils.log.LoggerMessages.*;
+
 @Component
 public class AppController implements CommandLineRunner {
+
 
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.printf("[AppController] Server running on port 8080...");
         System.out.printf("%s",new Date());
+        System.out.printf(SERVER_RUNNING_OK);
+
     }
 }
--- src/main/java/com/aviobrief/springserver/utils/log/LoggerMessages.java
+++ src/main/java/com/aviobrief/springserver/utils/log/LoggerMessages.java
@@ -1,4 +1,7 @@
 package com.aviobrief.springserver.utils.log;
 
 public class LoggerMessages {
+    /* dev */
+    public static final String SERVER_RUNNING_OK = "Server listening on port 8080...";
+
 }
