f9d65e03c495067bff9f7a4be67389b12f99710b
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-01-07 04:04:55 +0200

    save

--- src/main/java/com/aviobrief/springserver/devTools/AppController.java
+++ src/main/java/com/aviobrief/springserver/devTools/AppController.java
@@ -5,18 +5,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static com.aviobrief.springserver.utils.log.LoggerMessages.*;
 
 @Component
 public class AppController implements CommandLineRunner {
 
+    private Logger logger = Logger.getLogger("Test");
 
     @Override
     public void run(String... args) throws Exception {
 
         System.out.printf("%s",new Date());
         System.out.printf(SERVER_RUNNING_OK);
+        logger.log(Level.INFO, "test");
 
     }
 }
