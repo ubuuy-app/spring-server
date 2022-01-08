565c0b7d0cd85748334d5b020659e043f44f7b4a
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-01-08 04:49:24 +0200

    imported server port at runtime
    #3

--- src/main/java/com/aviobrief/springserver/devTools/AppController.java
+++ src/main/java/com/aviobrief/springserver/devTools/AppController.java
@@ -1,25 +1,24 @@
 package com.aviobrief.springserver.devTools;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import static com.aviobrief.springserver.utils.log.LoggerMessages.*;
 
 @Component
 public class AppController implements CommandLineRunner {
 
-    private Logger logger = Logger.getLogger("Test");
+    private Logger logger = Logger.getLogger("AppController");
+    private Environment environment;
+
+    public AppController(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-
-        System.out.printf("%s", new Date());
-        System.out.printf(SERVER_RUNNING_OK);
-        logger.log(Level.INFO, "test");
-
+        logger.log(Level.INFO, String.format(SERVER_RUNNING_OK_TEMPLATE,environment.getProperty("local.server.port")));
     }
 }
--- src/main/java/com/aviobrief/springserver/utils/log/LoggerMessages.java
+++ src/main/java/com/aviobrief/springserver/utils/log/LoggerMessages.java
@@ -1,7 +1,13 @@
 package com.aviobrief.springserver.utils.log;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
 public class LoggerMessages {
+
     /* dev */
-    public static final String SERVER_RUNNING_OK = "Server listening on port 8080...";
+    public static final String SERVER_RUNNING_OK_TEMPLATE = "Server listening on port %s...";
 
 }
