b865f0d26b9238726626d13d2be04c694cbfebfc
Petar-I-Petkov <cpt.petkov@gmail.com>
2022-02-09 06:18:17 +0200

    Back to online mode
    #42

--- src/main/java/com/ubuuy/springserver/web/AuthController.java
+++ src/main/java/com/ubuuy/springserver/web/AuthController.java
@@ -90,7 +90,7 @@ public class AuthController {
             HttpHeaders responseHeaders = authService.generateDoubleSubmitCookieHeader();
 
             /* ADD LOGIN TO USER HISTORY */
-//            authService.addLoginToUserHistory(loginRequest.email(), request, loginResponse.getAccessToken());
+            authService.addLoginToUserHistory(loginRequest.email(), request, loginResponse.getAccessToken());
 
             return ResponseEntity.ok()
                     .headers(responseHeaders)
@@ -125,7 +125,7 @@ public class AuthController {
 
     }
 
-    @PostMapping(path = "/api/auth/logout", produces = "application/json")
+    @GetMapping(path = "/api/auth/logout", produces = "application/json")
     public ResponseEntity<?> logout(HttpServletRequest request) {
 
         try {
